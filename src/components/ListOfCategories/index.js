15e891b601e1e7142f2a91c41ace6cbe07f9b073
Jorge Norbey Alejandro Gonzalez Montealegre <jorgenalejandrogm@gmail.com>
2020-12-13 21:09:36 -0500

    16. useCategoriesData

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://curso-platzi-react-avanzado-7zfbipf6k.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
+
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     const onScroll = e => {
@@ -26,11 +35,17 @@ export const ListOfCategories = () => {
   }, [showFixed])
 
   const renderList = (fixed) => {
-    <List className={fixed ? 'fixed' : ''}>
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        loading
+          ? <Item key='loading' />
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  }
+
+  if (loading) {
+    return 'Cargando...'
   }
 
   return (
