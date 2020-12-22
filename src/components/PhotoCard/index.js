9e9d50a9e9888b4e306092e661c43516331829fb
Jorge Norbey Alejandro Gonzalez Montealegre <jorgenalejandrogm@gmail.com>
2020-12-21 22:03:38 -0500

    19. Usando el localStorage para guardar los likes parte

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,11 +1,22 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null)
   const [show, setShow] = useState(false)
+  const key = `like-${id}`
+
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (e) {
+      return false
+    }
+  })
+
   useEffect(function () {
     Promise.resolve(typeof window.IntersectionObserver !== 'undefined'
       ? window.IntersectionObserver
@@ -22,6 +33,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     })
   }, [element])
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return (
     <Article ref={element}>
       {
@@ -33,8 +55,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} Likes!
+            <Button onClick={() => setLocalStorage(!liked)}>
+              <Icon size='32px' /> {likes} Likes!
             </Button>
           </>
       }
