0cda903986e69259c0a574e70db7d302206ad472
Jorge Norbey Alejandro Gonzalez Montealegre <jorgenalejandrogm@gmail.com>
2021-01-03 20:02:44 -0500

    26. ¿Qué es Reach Router? Creando la ruta Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 /* eslint-disable react/jsx-closing-tag-location */
-import React, { Fragment } from 'react'
-import { ListOfCategories } from './components/ListOfCategories/index'
+import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Home } from './pages/Home'
+import { Router } from '@reach/router'
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
@@ -18,10 +18,10 @@ export const App = () => {
       {
         detailId
           ? <PhotoCardWithQuery id={detailId} />
-          : <>
-            <ListOfCategories />
-            <ListOfPhotoCards categoryId={2} />
-          </>
+          : <Router>
+            <Home path='/' />
+            <Home path='/pet/:id' />
+          </Router>
       }
     </div>
   )
